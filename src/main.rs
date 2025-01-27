567e3e2b34e3dad98559c04137ef36e27094701d
JustlySold <jonatansvensson00@gmail.com>
2025-01-27 23:55:44 +0100

    The first read is possible

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-
+use std::env;
 use std::fs;
 fn main() {
-    todo!()
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+    
+    println!("In filepath, {file_path}");
+    let contents = fs::read_to_string(file_path).expect("Probably should be read now?");
+    println!("With text :\n {contents}")
 }
